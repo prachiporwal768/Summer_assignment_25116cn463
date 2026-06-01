1fea16807ced42d5392e8efd1b710310d031d3cd
prachiporwal768 <25116cn463@glbitm.ac.in>
2026-06-01 18:58:57 +0530

    sum of natural no.

---
+++ day 1 assignment.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &N);
+
+    for(int i = 1; i <= N; i++) {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", N, sum);
+
+    return 0;
+}
