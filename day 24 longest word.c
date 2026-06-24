e31dd57964ef4bd5f17abd6a64b7670cd5cfb521
prachiporwal768 <25116cn463@glbitm.ac.in>
2026-06-24 14:31:20 +0530

    longest word

---
+++ day 24 longest word.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    char longest[100];
+    int i = 0, start = 0;
+    int maxLen = 0, len = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (1) {
+        if (str[i] != ' ' && str[i] != '\0' && str[i] != '\n') {
+            len++;
+        } else {
+            if (len > maxLen) {
+                maxLen = len;
+                strncpy(longest, &str[start], len);
+                longest[len] = '\0';
+            }
+            len = 0;
+            start = i + 1;
+        }
+
+        if (str[i] == '\0')
+            break;
+
+        i++;
+    }
+
+    printf("Longest Word: %s\n", longest);
+    printf("Length: %d\n", maxLen);
+
+    return 0;
+}
